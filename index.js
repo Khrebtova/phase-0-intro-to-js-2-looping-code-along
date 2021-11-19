1da67a74040abfb6b409f0b827f04319e6f43f1d
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2021-11-19 13:45:44 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = "surprise"
+
+function writeCards(names, event) {
+    let messages =[];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+writeCards(names, event);
+
+function countDown() {
+    for (let n = 10; n >= 0; n--){
+        console.log(n);
+    }
+}
+countDown(10);
